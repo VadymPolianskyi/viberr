a8a0b7b6177130414642bf46a5a6f2b1bbfa0755
VadymPolyanski <vadym.polyanski@gmail.com>
2017-05-08 16:00:44 +0300

    created simple user service corrected album service

--- music/urls.py
+++ music/urls.py
@@ -1,4 +1,5 @@
 from django.conf.urls import url
+
 from . import views
 
 app_name = 'music'
@@ -6,6 +7,9 @@ app_name = 'music'
 urlpatterns = [
     #/music/
     url(r'^$', views.IndexView.as_view(), name='index'),
+
+    #/music/register/
+    url(r'^register/$', views.UserFormView.as_view(), name='register'),
 
     #/music/712/
     url(r'^(?P<pk>[0-9]+)/$', views.DetailView.as_view(), name='detail'),
